72ebfda450a0aa39a855f8c7c1863871bb690afc
Your Name <yourname@example.com>
2023-11-11 11:44:30 -0500

    Initial commit

---
+++ healthcare.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Read in the data
+healthcare = pd.read_csv('/Users/youssoufhassan/Desktop/healthcare/healthcare_dataset.csv')
+
+# Examine the data
+print(healthcare.head())
